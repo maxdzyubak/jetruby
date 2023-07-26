8094e5dd3968788fd7d53c1ca692fb181e6daaee
Maxim Dzyubak <maxdzyubak@gmail.com>
2023-07-26 14:16:08 +0300

    Update

--- lessons/lesson-3-block_schemas_and_data_types/if_word_end_on_cs.rb
+++ lessons/lesson-3-block_schemas_and_data_types/if_word_end_on_cs.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# * Task:
+# Написать скрипт, который будет принимать слово, если слово заканчивается на "CS" (в любом регистре):
+# - выводит на экран цифру 2 в степени (длины введенного слова) - если не заканчивается, — выводит слово задом наперёд
+
 def if_word_end_on_cs(word)
   last_two_letters = word[-2..]
   last_two_letters.casecmp('cs').zero? ? 2**word.length : word.reverse
